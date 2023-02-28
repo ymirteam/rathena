eb4b9b996bdba6c05e5120d23c7bfca126843f6e
PCM <59070443+iberryRO@users.noreply.github.com>
2023-02-28 14:08:25 +0700

    Create update server.bat

---
+++ update server.bat
@@ -0,0 +1,6 @@
+@ECHO OFF
+ECHO Getting the latest Git from the null webservice
+Echo Wait...
+git pull https://github.com/iberryRO/MiX.git
+Echo ---------------------------------------------------
+Set /p Wait="Finished."
