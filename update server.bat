b4d4f74a4f89dd63ce4012a4e6fc080f8d6c4cc4
N U L L <59070443+iberryRO@users.noreply.github.com>
2023-04-14 00:43:31 +0700

    Update update server.bat

--- update server.bat
+++ update server.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 ECHO Getting the latest Git from the null webservice
 Echo Wait...
-git pull https://github.com/iberryRO/MiX.git
+git pull https://github.com/iberryRO/MiX.git ex_Vending
 Echo ---------------------------------------------------
 Set /p Wait="Finished."
